b009f7cd6dbf29a851844bca99c41dd8e067d537
Meesh292 <michelle.dana@asu.edu>
2020-03-21 17:13:11 -0700

    Week 2

---
+++ Week 2.sql
@@ -0,0 +1,22 @@
+SHOW tables;
+DESC employees;
+DESC titles;
+desc salaries;
+desc departments;
+desc dept_emp;
+
+select count(*), t.title from employees e
+inner join titles t on t.emp_no = e.emp_no
+where birth_date > '1965-01-01'
+group by title;
+
+select t.title, avg(salary) from salaries s 
+inner join titles t on t.emp_no = s.emp_no 
+group by title;
+
+select d.dept_name, sum(salary) from dept_emp de 
+inner join departments d on d.dept_no = de.dept_no 
+inner join salaries s on s.emp_no = de.emp_no 
+where s.from_date >= '1990-01-01' and s.to_date <='1992-12-31'
+group by d.dept_name;
+
